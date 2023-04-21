929378f2e13d2eacace51433c40ca3f1c29d7cc0
JeevanMKJ <jeevanjones.jmkj@gmail.com>
2023-04-21 14:17:43 -0500

    added Gatsby.js, React.js and Tailwind landing page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
